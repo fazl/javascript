4cb155bf7b5edd5f4aa91fc5389e571067692528
fazl <5173704+fazl@users.noreply.github.com>
2019-04-01 16:02:41 +0300

    Introduce CSS and script into plain html page.

--- reintro/src/meta-example-script.js
+++ reintro/src/meta-example-script.js
@@ -1,10 +1,10 @@
-var list = document.createElement('ul');
-var info = document.createElement('p');
-var html = document.querySelector('html');
+var list = document.createElement('ul');  //(u??) list ?? <ul></ul>
+var info = document.createElement('p');   //para ??           <p></p>
+var html = document.querySelector('html');//root element ??   <html></html>
 
 info.textContent = 'Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
 
-document.body.appendChild(info);
+document.body.appendChild(info);                   //will 'view source' reveal changes?
 document.body.appendChild(list);
 
 html.onclick = function() {
@@ -14,8 +14,8 @@ html.onclick = function() {
   list.appendChild(listItem);
 
   listItem.onclick = function(e) {
-    e.stopPropagation();
+    e.stopPropagation();          //??probly means doesn't need to be handled in containing elems??
     var listContent = prompt('Enter new content for your list item');
     this.textContent = listContent;
-  }
+  };
 }
